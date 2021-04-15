6c0b6c37b1e1dcf282e65c1cc0a19f5181feef90
zod <jean-baptiste.dupuy@epitech.eu>
2021-04-15 20:50:34 +0200

    6

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def train(epoch, warmup_factor, model, optimizer, dataloader):
         recon_batch, mu, logvar = model(data)
         loss = model.loss_function(recon_batch, data, mu, logvar, warmup_factor)
         loss.backward()
-        torch.nn.utils.clip_grad_value_(model.parameters(), 10000.0)
+        torch.nn.utils.clip_grad_value_(model.parameters(), 1.0)
         train_loss += loss.item()
         optimizer.step()
         if batch_idx % LOG_INTERVAL == 0:

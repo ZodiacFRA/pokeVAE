2d1d48ffbfdb49d0403403bd94d1f60a3df3e106
zod <jean-baptiste.dupuy@epitech.eu>
2021-04-15 21:02:11 +0200

    2

--- VAE.py
+++ VAE.py
@@ -54,8 +54,8 @@ class VAE(torch.nn.Module):
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        print("KLD", KLD)
         res = warmup_factor * KLD + BCE
+        res = min(res, 50_000)
         return res
 
 
